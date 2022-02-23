4f12778a4f3699eb93d9ad1669ccf179fe0afb78
KAMONPAN KAEWHEANG <kamonpan@dit.daikin.co.jp>
2022-02-23 21:52:43 +0700

    Initial Commit

---
+++ iOS13-Uikit-goodNews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS13-Uikit-goodNews
+//
+//  Created by admin on 23/2/2565 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
