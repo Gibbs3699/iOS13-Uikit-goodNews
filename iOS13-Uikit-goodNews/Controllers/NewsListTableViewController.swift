5e4776f66e3e753f07e950a87aac176ace911b10
Gibbs3699 <gift_kamonpan@hotmail.com>
2022-02-27 23:31:20 +0700

    Displaying News

--- iOS13-Uikit-goodNews/Controllers/NewsListTableViewController.swift
+++ iOS13-Uikit-goodNews/Controllers/NewsListTableViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class NewsListTableViewController: UITableViewController {
     
+    private var articleListVM: ArticleListViewModel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setup()
@@ -20,8 +22,37 @@ class NewsListTableViewController: UITableViewController {
         
         let url = URL(string: "https://newsapi.org/v2/top-headlines?country=us&apiKey=14c29b283d174368809f0ba47d038907")!
         
-        Webservice().getArticles(url: url) {_ in
-            
+        Webservice().getArticles(url: url) { aritcles in
+            if let aritcles = aritcles {
+                
+                self.articleListVM = ArticleListViewModel(articles: aritcles)
+                
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
+            }
         }
     }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return self.articleListVM == nil ? 0 : self.articleListVM.numberOfSections
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.articleListVM.numberOfRowsInSection(section)
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "ArticleTableViewCell", for: indexPath) as? ArticleTableViewCell else {
+            fatalError("ArticleTableViewCell not found")
+        }
+        
+        let articleVM = self.articleListVM.articleAtIndex(indexPath.row)
+        
+        cell.titleLabel.text = articleVM.title
+        cell.descriptionLabel.text = articleVM.description
+        return cell
+    }
+    
 }
