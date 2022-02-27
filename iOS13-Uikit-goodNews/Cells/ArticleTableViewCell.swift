e5a881ef53d0f50e4db9cfcd98163165dc5efd98
Gibbs3699 <gift_kamonpan@hotmail.com>
2022-02-27 23:01:23 +0700

    Create Custom Self Sizing Cells

---
+++ iOS13-Uikit-goodNews/Cells/ArticleTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  ArticleTableViewCell.swift
+//  iOS13-Uikit-goodNews
+//
+//  Created by admin on 27/2/2565 BE.
+//
+
+import Foundation
+import UIKit
+
+class ArticleTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+}
